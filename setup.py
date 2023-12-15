4e700c4e591b6cb9570af97fe0f41d0a0cb409e7
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-12-15 14:00:59 +0100

    fix pyproject.toml

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import Extension, setup
+from distutils.core import Extension, setup
 
 setup_args = dict(ext_modules=[Extension("monitor", ["monitor/monitor.c"])])
 setup(**setup_args)
